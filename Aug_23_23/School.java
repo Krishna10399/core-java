2b8474788442208fdf1316994f24827984728864
Krishna10399 <131844946+Krishna10399@users.noreply.github.com>
2023-08-23 09:21:01 +0530

    Add files via upload

---
+++ Aug_23_23/School.java
@@ -0,0 +1,27 @@
+package Aug_23_23;
+
+public class School {
+    String studentName;
+    int age;
+    int mobileNo;
+
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+}
---
+++ Aug_23_23/SchoolMain.java
@@ -0,0 +1,15 @@
+package Aug_23_23;
+
+public class SchoolMain {
+    public static void main(String[] args) {
+        School sc = new School();
+        sc.setStudentName("joy");
+        sc.setAge(25);
+        sc.setMobileNo(485232168);
+        System.out.println(sc.getStudentName());
+        System.out.println(sc.getAge());
+        System.out.println(sc.getMobileNo());
+
+
+    }
+}
